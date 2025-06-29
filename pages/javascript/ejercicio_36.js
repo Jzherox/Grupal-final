a0f8636cf96351210fae1e327931b608b79e82e0
Jzherox <eliasballadares2004@gmail.com>
2025-06-29 16:02:05 -0500

    fase 2

--- pages/javascript/ejercicio_36.js
+++ pages/javascript/ejercicio_36.js
@@ -7,12 +7,15 @@ function mostrar(texto) {
 function calcular() {
     resultado.innerHTML = ''
 
-    const numeros = (document.getElementById('numeros').value).split(';').map(Number)
+    let numeros = (document.getElementById('numeros').value).split(';').map(Number)
     let cubos = []
 
     for (let i = 0; i < numeros.length; i++) {
         cubos[i] = Math.pow(numeros[i], 3)
     }
+
+    numeros = numeros.join(", ")
+    cubos = cubos.join(", ")
 
     mostrar(`Tus valores son: '${numeros}'`)
     mostrar(`Tus valores al cubo son: '${cubos}'`)
